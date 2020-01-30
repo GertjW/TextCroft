5ab0f28cc7aa3d6b401189a49e79dcbf73fec326
GertjW <58505029+GertjW@users.noreply.github.com>
2020-01-30 13:41:10 +0100

    Add files via upload

--- Game.java
+++ Game.java
@@ -2,24 +2,24 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.*;
 import java.lang.reflect.*;
+
 /**
- *  This class is the main class of the "World of Zuul" application. 
- *  "World of Zuul" is a very simple, text based adventure game.  Users 
- *  can walk around some scenery. That's all. It should really be extended 
- *  to make it more interesting!
- * 
- *  To play this game, create an instance of this class and call the "play"
- *  method.
- * 
- *  This main class creates and initialises all the others: it creates all
- *  rooms, creates the parser and starts the game.  It also evaluates and
- *  executes the commands that the parser returns.
- * 
- * @author  Michael Kölling and David J. Barnes
+ * This class is the main class of the "World of Zuul" application.
+ * "World of Zuul" is a very simple, text based adventure game.  Users
+ * can walk around some scenery. That's all. It should really be extended
+ * to make it more interesting!
+ * <p>
+ * To play this game, create an instance of this class and call the "play"
+ * method.
+ * <p>
+ * This main class creates and initialises all the others: it creates all
+ * rooms, creates the parser and starts the game.  It also evaluates and
+ * executes the commands that the parser returns.
+ *
+ * @author Michael Kölling and David J. Barnes
  * @version 2016.02.29
  */
-
-public class Game 
+public class Game
 {
     private Parser parser;
     private Room currentRoom;
@@ -35,7 +35,7 @@ public class Game
     private Stack<Integer> stack = new Stack<Integer>();
 
     /**
-     * Create the game and initialise its internal map, and set limit of moves, powerLevel and inventory.
+     * Create the game and initialise its internal map.
      */
     public Game() 
     {
@@ -47,29 +47,16 @@ public class Game
         numberOfMoves = 0;
         limitOfMoves = 50;
     }
-    
-    public static void main (String[] args)
-    {
-        Game game = new Game();
-        game.play();
-    }
 
     /**
-     * Create all the rooms, put the rooms in a hashmap and link their exits together.
+     * Create all the rooms and link their exits together.
      */
     private void createRooms()
     {
         Room forest, winter, desert, jungle, jungle_tempel, village, savanna, house, cave, mineshaft, mineshaft2, end, nether1, nether2, nether3, nether4, nether5, nether6, nether7, nether8;
 
-        // create the rooms
+        // create the rooms, with room id and itemID
 
-        //wood = 1
-        //sand = 2
-        //stone = 3
-        //diamond = 4
-        //iron 5
-
-        //null = 0
         rooms.put(1, forest = new Room("You are now in the forest", 1, 1));
         rooms.put(2, winter= new Room("You are in a winter biome", 2, 1));
         rooms.put(3, desert = new Room("This is a sandy place, looks like a desert", 3, 2));
@@ -82,14 +69,15 @@ public class Game
         rooms.put(10, mineshaft= new Room("This is a MineShaft", 10, 5));
         rooms.put(11, mineshaft2= new Room("You are really close to the end now, once you are in the end, you cant go back. Go east if you want to go to the end", 18, 4));
         rooms.put(12, end = new Room("This is the end, kill the dragon and finish the game. Before you fight him, equip your gear and use all your potions!", 11, 0));
-        rooms.put(13, nether1 = new Room("You are now in the Nether. Here are a couple of chests that you can only open with a diamond pickaxe", 12, 0));
-        rooms.put(14, nether2 = new Room("You are now in the Nether", 13, 20));
-        rooms.put(15, nether3 = new Room("You are now in the Nether", 14, 0));
-        rooms.put(16, nether4 = new Room("You are now in the Nether", 15, 0));
-        rooms.put(17,nether5 = new Room("That looks like an Nether Wither, try to fight him or run away", 16, 0));
-        rooms.put(18,nether6 = new Room("You are now in the Nether", 17, 0));
-        rooms.put(19, nether7 = new Room("You are now in the Nether", 18, 0));
-        rooms.put(20, nether8 = new Room("There is a chest on the ground, but there is also a portal on your west side. You sure you wanna leave?", 19, 21));
+        rooms.put(13, nether1 = new Room("You are now in the Nether. Here are a couple of chests that you can only open with a diamond pickaxe", 13, 0));
+        rooms.put(14, nether2 = new Room("You are now in the Nether, look a chest!", 14, 20));
+        rooms.put(15, nether3 = new Room("You are now in the Nether", 15, 0));
+        rooms.put(16, nether4 = new Room("You are now in the Nether", 16, 0));
+        rooms.put(17,nether5 = new Room("That looks like an Nether Wither, try to fight him or run away", 17, 0));
+        rooms.put(18,nether6 = new Room("You are now in the Nether", 18, 0));
+        rooms.put(20, nether7 = new Room("There is a chest on the ground, but there is also a portal on your west side. You sure you wanna leave?", 20, 21));
+        rooms.put(19, nether8 = new Room("You are now in the Nether", 19, 0));
+
 
         // initialise room exits
         forest.setExit("north", village);
@@ -129,34 +117,37 @@ public class Game
         nether1.setExit("east", nether3);
         nether1.setExit("south", nether4);
 
-        nether2.setExit("east", nether5);
+        nether2.setExit("east", nether8);
         nether2.setExit("south", nether1);
 
-        nether3.setExit("north", nether5);
+
+        nether3.setExit("north", nether8);
         nether3.setExit("east", nether6);
-        nether3.setExit("east", nether7);
         nether3.setExit("west", nether1);
 
         nether4.setExit("north", nether1);
-        nether4.setExit("south", nether8);
+        nether4.setExit("south", nether7);
 
-        nether5.setExit("south", nether3);
-        nether5.setExit("west", nether2);
+        nether5.setExit("south", nether6);
+        nether5.setExit("west", nether8);
 
-        nether6.setExit("south", savanna);
+        nether6.setExit("north", nether5);
+        nether6.setExit("east", savanna);
         nether6.setExit("west", nether3);
 
-        nether7.setExit("north", nether3);
-        nether7.setExit("west", nether4);
+        nether8.setExit("east", nether5);
+        nether8.setExit("south", nether3);
+        nether8.setExit("west", nether2);
 
-        nether8.setExit("north", nether4);
-        nether8.setExit("west", jungle);
 
-            
+        nether7.setExit("north", nether4);
+        nether7.setExit("west", jungle);
+
         house.setExit("south", savanna);
-        house.setExit("west", nether1);
+        house.setExit("east", nether1);
 
         currentRoom = forest;  // start game outside     
+        pushStack(stack, currentRoom);
     }
 
     /**
@@ -190,7 +181,7 @@ public class Game
         System.out.println("You can open your challenge list by using the command: challenges");
         System.out.println("If you completed a challenge, you will get a message");
         System.out.println();
- 
+        System.out.println("If you want to get some items, try to use your hand by the command: use hand");
         inv.showCraftingTableStatus();
         inv.printPowerLvl();
         System.out.println();
@@ -230,7 +221,6 @@ public class Game
         }
         else if (commandWord.equals("items")){
             ItemToGet();
-
         }
         else if (commandWord.equals("inv")){
             this.inv.showInv();
@@ -243,11 +233,7 @@ public class Game
                 System.out.println("Je kan niet verder terug");
                 System.out.println(currentRoom.getLongDescription());
             } else {
-            //if (popStack(stack, currentRoom) != 0) {
                 goRoom(popStack(stack, currentRoom));
-                
-                
-            //} else {
             }
         }
         else if (commandWord.equals("equip")){
@@ -264,13 +250,11 @@ public class Game
         return wantToQuit;
     }
 
+    // implementations of user commands:
 
     /**
-     * Print out which item can be picked up in which particular room.
-     * else print else message
-     * 
-     */ 
-
+     *
+     */
     private void ItemToGet(){
         if(currentRoom.getRoomID()==(1)){
             System.out.println("You can get here " + currentRoom.getItemName());}
@@ -290,11 +274,12 @@ public class Game
             System.out.println("You can get here " + currentRoom.getItemName());}
         else{System.out.println("You can't get anything here");}
     }
+
     /**
      * Print out some help information.
-     * Here we print some stupid, cryptic message and a list of the 
+     * Here we print some stupid, cryptic message and a list of the
      * command words.
-     */ 
+     */
     private void printHelp() 
     {
         System.out.println(currentRoom.getLongDescription());
@@ -306,7 +291,7 @@ public class Game
         inv.printPowerLvl();
         System.out.println();
         System.out.println("Your command words are:");
-        System.out.println("go quit help use items inv back craft equip fight");
+        System.out.println("go quit help use items inv back craft use equip fight");
         if(currentRoom.getRoomID()==(11)){
             System.out.println("You can fight the Ender Dragon by using the command: fight");
         }
@@ -338,6 +323,7 @@ public class Game
             System.out.println(currentRoom.getLongDescription());
         }
     }
+
     private void goRoom(int roomID) 
     {
         currentRoom = rooms.get(roomID);
@@ -367,25 +353,25 @@ public class Game
     private void pushStack(Stack stack, Room currentRoom) {
         stack.push(currentRoom.getRoomID());
     }
-    
-     private int popStack(Stack stack, Room currentRoom){
-                
-       if(stack.size() == 1){
-           return 0;
+
+    private int popStack(Stack stack, Room currentRoom){
+
+        if(stack.size() == 1){
+            return 0;
         } else {
             stack.pop();
             return (Integer) stack.peek();
-       }
+        }
     }
-    /**
+ /**
      *  @param item = int i                             
      *  do a ++ on the item from the room where you were in.
      * 
      */
-
     private void increment(int i){
         this.inv.increment(currentRoom.getItemID(), i);
     }
+    
     /**
      * @param secondWord
      * if the second word is an item craft it
@@ -444,11 +430,10 @@ public class Game
                 System.out.println("Try it there");}
         }
 
-    
         else{System.out.println("You cant craft this");
         }
     }
-    /**
+/**
      * @param second word
      * If the second word is an item that is useable, use the item
      * else ask what he wants to use
@@ -460,28 +445,32 @@ public class Game
             return;
         }
         String itemToUse = command.getSecondWord();
-        if(itemToUse.equals("hand") && currentRoom.getItemID() == 1){
-            increment(1);
-            System.out.println("Wood: +1");
-        } else if(currentRoom.getItemID() != 1){
-            System.out.println("You can't use that here"); 
-        }
+        if(itemToUse.equals("hand"))
+            if(currentRoom.getItemID() == 1){
+                increment(1);
+                System.out.println("Wood: +1");
+            } else{
+                System.out.println("You can't use that here"); }
 
-        if(itemToUse.equals("wood_pickaxe") && currentRoom.getItemID() == 3){
-            inv.use(7);
-        }else if(currentRoom.getItemID() != 3){
-                     System.out.println("You can't use that here");
-                 }
-        if(itemToUse.equals("stone_axe") && currentRoom.getItemID() == 1){
-            inv.use(9);
-        }else if(currentRoom.getItemID() != 1){
-                     System.out.println("You can't use that here");
-                 }
-        if(itemToUse.equals("iron_pickaxe") && currentRoom.getItemID() == 5){
-            inv.use(11);
-        }else if(currentRoom.getItemID() != 5){
-                     System.out.println("You can't use that here");
-                 }
+        if(itemToUse.equals("wood_pickaxe")) 
+            if(currentRoom.getItemID() == 3){
+                inv.use(7);
+            }else{//if(currentRoom.getItemID() != 3){
+                System.out.println("You can't use that here");
+            }
+
+        else if(itemToUse.equals("stone_axe"))
+            if(currentRoom.getItemID() == 1){
+                inv.use(9);
+            }else{
+                System.out.println("You can't use that here");
+            }
+        if(itemToUse.equals("iron_pickaxe"))
+            if(currentRoom.getItemID() == 5){
+                inv.use(11);
+            }else{
+                System.out.println("You can't use that here");
+            }
         if(itemToUse.equals("diamond_pickaxe"))
             if(currentRoom.getItemID() == 4){
                 inv.use(18);}
@@ -501,8 +490,9 @@ public class Game
         }
         if(itemToUse.equals("dev")){
             inv.use(999);
-        }
-    }
+        } 
+    
+}
     /**
      * if the room ID has a boss you can fight it.
      * else There is nothing to fight
@@ -518,18 +508,17 @@ public class Game
             this.inv.fightBoss("nether_wither");
         }else{System.out.println("There is nothing to fight");
         }
-    
-   }
-   /**
+
+    }
+    /**
     * count move at every goroom command
     * print current moves and moves left
     * if maximum is reached print game over
     */
-   
-   public static boolean countMove(){
+    public static boolean countMove(){
         // tel de stappen
         numberOfMoves++;
-        
+
         // Informatie over hoeveel stappen je hebt gezet / over hebt
         if (numberOfMoves < limitOfMoves) {
             System.out.println("Current number of moves : " + numberOfMoves);
@@ -542,7 +531,7 @@ public class Game
             System.out.println();
             System.out.println();
             System.exit(0);
-            
+
             return true;
         }
     }
